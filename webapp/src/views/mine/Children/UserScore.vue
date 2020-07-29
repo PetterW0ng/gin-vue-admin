244a9f6305b2f8db96eacf330787858f197b3ff9
laoW0ng <wangqin@lkmotion.com>
2020-07-29 11:25:16 +0800

    生成用户的分享图片

--- webapp/src/views/mine/Children/UserScore.vue
+++ webapp/src/views/mine/Children/UserScore.vue
@@ -4,7 +4,7 @@
                      :fixed=true
                      :border=false
                      style="height: 2.5rem;padding-top:0.5rem"/>
-        <div class="mainDiv">
+        <div class="mainDiv" v-show="!showImageWrapper">
             <!-- 雷达图 -->
             <div id="radarChartDiv" style="width: 100%;height: 400px"></div>
             <div class="recomand">
@@ -41,18 +41,38 @@
                 </div>
             </div>
         </div>
+        <div ref="imageWrapper" class="shareBgDiv" v-show="!showImageWrapper">
+            <!-- 背景图 -->
+            <div style="height: 100%;width: 100%">
+                <img src="../../../images/mine/shareQr.jpg" style="height: 100%;width: 100%"/>
+            </div>
+            <!-- 雷达图 -->
+            <div class="radarChartDiv2">
+                <div id="radarChartDiv2" style="position: relative;height: 100%"></div>
+            </div>
+        </div>
+        <div v-show="dialogTableVisible">
+            <img :src="imgUrl" width="100%"/>
+        </div>
+        <van-button v-show="!showImageWrapper" class="button" type="info" size="large" @click="toImage()">
+            生成图片
+        </van-button>
     </div>
 </template>
 
 <script>
     import {userScore, toStore} from "../../../serve/api";
     import echarts from 'echarts'
+    import html2canvas from "html2canvas"
     import {Toast} from 'vant'
 
     export default {
         name: "UserScore",
         data() {
             return {
+                imgUrl: '',
+                showImageWrapper: false,
+                dialogTableVisible: false,
                 gtStandard: [],
                 ltStandard: [],
                 topicRelatedList: [],
@@ -184,6 +204,135 @@
                             /*[60, 60, 65, 60, 70, 40, 80, 63, 68, 60, 77, 60, 80, 62, 80]*/
                         ]
                     }]
+                },
+                option1: {
+                    backgroundColor: 'white',
+                    "normal": {
+                        "top": 200,
+                        "left": 300,
+                        "width": 500,
+                        "height": 400,
+                        "zIndex": 6,
+                        "backgroundColor": ""
+                    },
+                    "color": ["rgba(245, 166, 35, 1)", "rgba(19, 173, 255, 1)"],
+                    "title": {
+                        "show": false,
+                        "text": "测评结果",
+                        "left": "80%",
+                        "top": "2%",
+                        "textStyle": {
+                            "fontSize": 14,
+                            "color": "black",
+                            "fontStyle": "normal",
+                            "fontWeight": "normal"
+                        }
+                    },
+                    "tooltip": {
+                        "show": false,
+                        "trigger": "item"
+                    },
+                    "legend": {
+                        "show": true,
+                        "icon": "circle",
+                        itemHeight: 8,
+                        "left": "80%",
+                        "top": "4%",
+                        "orient": "horizontal",
+                        "textStyle": {
+                            "fontSize": 8,
+                            "color": "black"
+                        },
+                        "data": ["基准分", "所得分"]
+                    },
+                    "radar": {
+                        "center": ["50%", "50%"],
+                        "radius": "63%",
+                        "startAngle": 90,
+                        "splitNumber": 4,
+                        "shape": 'angle', // 拐点的样式，还可以取值'rect','angle'等
+                        "splitArea": {
+                            "areaStyle": {
+                                "color": ["transparent"]
+                            }
+                        },
+                        "axisLabel": {
+                            "show": false,
+                            "fontSize": 8,
+                            "color": "#fff",
+                            "fontStyle": "normal",
+                            "fontWeight": "normal"
+                        },
+                        "axisLine": {
+                            "show": true,
+                            "lineStyle": {
+                                "color": "grey" //
+                            }
+                        },
+                        "splitLine": {
+                            "show": true,
+                            "lineStyle": {
+                                "color": "grey" //
+                            }
+                        },
+                        "indicator": [/*{
+                            "name": "洪水",
+                            "max": 88
+                        }*/]
+                    },
+                    "series": [{
+                        "name": "基准分",
+                        "type": "radar",
+                        "symbol": "circle",
+                        "symbolSize": 5,
+                        "areaStyle": {
+                            "normal": {
+                                "color": "rgba(245, 166, 35, 0.4)"
+                            }
+                        },
+                        itemStyle: {
+                            color: 'rgba(245, 166, 35, 1)',
+                            borderColor: 'rgba(245, 166, 35, 0.3)',
+                            borderWidth: 5,
+                        },
+                        "lineStyle": {
+                            "normal": {
+                                "type": "dashed",
+                                "color": "rgba(245, 166, 35, 1)",
+                                "width": 2
+                            }
+                        },
+                        "data": [
+                            /*[80, 50, 55, 80, 50, 80, 48, 43, 60, 78, 60, 40, 42, 44, 65]*/
+                        ]
+                    }, {
+                        "name": "所得分",
+                        "type": "radar",
+                        "symbol": "circle",
+                        "symbolSize": 5,
+                        "itemStyle": {
+                            "normal": {
+                                color: 'rgba(19, 173, 255, 1)',
+                                "borderColor": "rgba(19, 173, 255, 0.4)",
+                                "borderWidth": 5
+                            }
+                        },
+                        "areaStyle": {
+                            "normal": {
+                                "color": "rgba(19, 173, 255, 0.5)"
+                            }
+                        },
+                        "lineStyle": {
+                            "normal": {
+                                "color": "rgba(19, 173, 255, 1)",
+                                "width": 2,
+                                "type": "dashed"
+                            }
+                        },
+                        "data": [
+                            /*[60, 60, 65, 60, 70, 40, 80, 63, 68, 60, 77, 60, 80, 62, 80]*/
+                        ]
+                    }]
                 }
             }
         },
@@ -274,12 +423,32 @@
                 }
                 indicator.forEach((e) => {
                     this.option.radar.indicator.push({name: e.substr(0, 6) + "\n" + e.substr(6, e.length), max: 10})
+                    this.option1.radar.indicator.push({name: e.substr(0, 6) + "\n" + e.substr(6, e.length), max: 10})
                 })
                 this.option.series[0]["data"].push(result.data.standardScores)
                 this.option.series[1]["data"].push(result.data.userScores)
+
+                this.option1.series[0]["data"].push(result.data.standardScores)
+                this.option1.series[1]["data"].push(result.data.userScores)
                 let radarChart = echarts.init(document.getElementById("radarChartDiv"))
                 radarChart.setOption(this.option)
+                let radarChart2 = echarts.init(document.getElementById("radarChartDiv2"))
+                radarChart2.setOption(this.option1)
             },
+            toImage() {
+                window.pageYOffset = 0;
+                document.documentElement.scrollTop = 0;
+                document.body.scrollTop = 0;
+                // this.showImageWrapper = true;
+                html2canvas(this.$refs.imageWrapper).then(canvas => {
+                    let dataURL = canvas.toDataURL("image/png");
+                    this.imgUrl = dataURL;
+                    if (this.imgUrl !== "") {
+                        this.dialogTableVisible = true;
+                        this.showImageWrapper = true;
+                    }
+                });
+            }
         }
     }
 </script>
@@ -287,6 +456,22 @@
 <style lang="less" scoped>
     .van-nav-bar__title {
         font-size: 1rem;
+    }
+
+    .shareBgDiv {
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
+
+    .radarChartDiv2 {
+        position: absolute;
+        width: 335px;
+        height: 270px;
+        z-index: 1;
+        top: 50%;
+        left: 10%;
+        transform: translateY(-50%);
     }
 
     .mainDiv {
@@ -298,7 +483,6 @@
                 color: #108EE9;
                 font-weight: bold;
             }
-        ;
 
             .title {
                 padding: 0.8rem 1rem;
@@ -309,5 +493,6 @@
                 padding: 0rem 0.85rem;
             }
         }
+
     }
 </style>
